ced8127d0ea34e43cc5e186d2d0b471808cc2682
Alymbek Sharabidinov <73030640+sharabidinov@users.noreply.github.com>
2024-02-22 06:24:51 +0600

    solution

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,20 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InterfaceCreator {
+    public static void main(String[] args) {
+
+    }
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return (list) -> {
+            List<Integer> result = new ArrayList<>();
+            for (Integer integer : list) {
+                result.add(integer / divider);
+            }
+            return result;
+        };
     }
 }
